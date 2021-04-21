b84f1f19597843aec6ad34dead254d5eb904e599
Dmitry Komaldinov <dk@qrator.net>
2021-04-21 17:15:58 +0300

    add NOT, OR, Prop checks; a scratch of constexpr string parser

--- main.cpp
+++ main.cpp
@@ -21,6 +21,40 @@ struct List<H, Ts...> {
     using Tail = List<Ts...>;
 };
 
+template <typename, typename>
+struct ListContains {
+    constexpr const static bool value = false;
+};
+
+template <typename... Elements, typename Item>
+struct ListContains<List<Elements...>, Item> {
+    constexpr const static bool value = (false || ... || std::is_same_v<Elements, Item>);
+};
+
+template <typename L, typename>
+struct ListPushFront {
+    using type = L;
+};
+
+template <typename Item, typename... Elements>
+struct ListPushFront<List<Elements...>, Item> {
+    using type = List<Item, Elements...>;
+};
+
+template <typename Lhs, typename>
+struct ListMinus {
+    using type = Lhs;
+};
+
+template <typename Head, typename... Tail, typename Rhs>
+struct ListMinus<List<Head, Tail...>, Rhs> {
+    using type = std::conditional_t<
+        ListContains<Rhs, Head>::value,
+        typename ListMinus<List<Tail...>, Rhs>::type,
+        typename ListPushFront<typename ListMinus<List<Tail...>, Rhs>::type, Head>::type
+    >;
+};
+
 template <typename K, typename V>
 struct MapItem {
     using Key = K;
@@ -71,7 +105,6 @@ Prop<std::integer_sequence<CharT, name...>> operator""_prop() {
     return {};
 }
 
-
 template <typename S, typename S0, typename R, typename L>
 struct KripkeModel {
     using States = S;
@@ -84,6 +117,172 @@ template <typename>
 struct ShowType {
     explicit ShowType() = delete;
 };
+
+template <typename...>
+struct True {};
+
+template <typename L, typename R>
+struct Or {
+    using Lhs = L;
+    using Rhs = R;
+};
+
+template <typename F>
+struct Not {
+    using Arg = F;
+};
+
+template <typename F>
+struct EG {
+    using Arg = F;
+};
+
+template <typename F>
+struct EU {
+    using Arg = F;
+};
+
+template <typename F>
+struct EX {
+    using Arg = F;
+};
+
+template <typename...>
+struct always_false {
+    constexpr static const inline bool value = false;
+};
+
+template <char first, typename Sequence>
+struct push_front {};
+
+template <char first, char... tail>
+struct push_front<first, std::integer_sequence<char, tail...>> {
+    using type = std::integer_sequence<char, first, tail...>;
+};
+
+template <char first, char...>
+struct cut_last {
+    constexpr const static char last = first;
+    using head = std::integer_sequence<char>;
+};
+
+template <char first, char second, char... tail>
+struct cut_last<first, second, tail...> {
+    constexpr const static char last = cut_last<second, tail...>::last;
+    using head = typename push_front<first, typename cut_last<second, tail...>::head>::type;
+};
+
+template <char first, char... chars>
+auto remove_braces() {
+    if constexpr (!sizeof...(chars)) {
+        return std::integer_sequence<char, first>{};
+    }
+    constexpr const char last = cut_last<chars...>::last;
+    if constexpr ((first == '(' && last == ')')
+               || (first == '[' && last == ']')
+               || (first == '{' && last == '}')) {
+        return typename cut_last<chars...>::head{};
+    } else {
+        static_assert(
+            always_false<std::integer_sequence<char, first, chars...>>::value);
+    }
+}
+
+template <typename>
+struct CTL {};
+
+template <typename CharT, CharT... chars>
+struct CTLParser {
+    using type = Prop<std::integer_sequence<CharT, chars...>>;
+};
+
+template <char... chars>
+struct CTLParser<char, '!', chars...> {
+    using type = Not<typename CTL<decltype(remove_braces<chars...>())>::type>;
+};
+
+template <char... chars>
+struct CTLParser<char, 'E', 'G', chars...> {
+    using type = EG<typename CTL<decltype(remove_braces<chars...>())>::type>;
+};
+
+template <char... chars>
+struct CTLParser<char, 'E', 'X', chars...> {
+    using type = EX<typename CTL<decltype(remove_braces<chars...>())>::type>;
+};
+
+template <char... chars>
+struct CTLParser<char, 'E', 'U', chars...> {
+    using type = EU<typename CTL<decltype(remove_braces<chars...>())>::type>;
+};
+
+template <typename CharT, CharT... chars>
+struct CTL<std::integer_sequence<CharT, chars...>> {
+    using type = typename CTLParser<CharT, chars...>::type;
+};
+
+template <typename CharT, CharT... name>
+typename CTL<std::integer_sequence<CharT, name...>>::type operator""_CTL() {
+    return {};
+}
+
+template <typename Model, typename CTLFormula>
+struct CTLCheck {
+    using Satisfy = typename Model::States;
+};
+
+template <typename L, template <typename> class Selector>
+struct ListSelect {
+    using type = List<>;
+};
+
+template <typename Head, typename... Tail, template <typename> class Selector>
+struct ListSelect<List<Head, Tail...>, Selector> {
+    using T = typename ListSelect<List<Tail...>, Selector>::type;
+    using type = std::conditional_t<
+        Selector<Head>::value,
+        typename ListPushFront<T, Head>::type,
+        T
+    >;
+};
+
+template <typename Model, typename Name>
+struct CTLCheck<Model, Prop<Name>> {
+    template <typename Item>
+    struct Selector {
+        using L = typename Model::Labels::template get<Item>;
+        constexpr const static bool value = ListContains<L, Prop<Name>>::value;
+    };
+    using Satisfy = typename ListSelect<typename Model::States, Selector>::type;
+};
+
+template <typename Lhs, typename>
+struct ListUnite {
+    using type = Lhs;
+};
+
+template <typename Lhs, typename Head, typename... Tail>
+struct ListUnite<Lhs, List<Head, Tail...>> {
+    using L = typename ListUnite<Lhs, List<Tail...>>::type;
+    using type = std::conditional_t<
+        ListContains<Lhs, Head>::value,
+        L,
+        typename ListPushFront<L, Head>::type>;
+};
+
+template <typename Model, typename Lhs, typename Rhs>
+struct CTLCheck<Model, Or<Lhs, Rhs>> {
+    using SatisfyLhs = typename CTLCheck<Model, Lhs>::Satisfy;
+    using SatisfyRhs = typename CTLCheck<Model, Rhs>::Satisfy;
+    using Satisfy = typename ListUnite<SatisfyLhs, SatisfyRhs>::type;
+};
+
+template <typename Model, typename F>
+struct CTLCheck<Model, Not<F>> {
+    using Satisfy = typename ListMinus<typename Model::States, typename CTLCheck<Model, F>::Satisfy>::type;
+};
+
+
 int main() {
     using my_list = List<Node<1>, Node<2>>;
     using my_dict = Dict<
@@ -91,22 +290,24 @@ int main() {
         MapItem<Node<2>, my_list>
     >;
 
-    std::cout << decltype("kek"_prop)::name << std::endl;
+    using p = decltype("p"_prop);
+    using q = decltype("q"_prop);
+    using r = decltype("r"_prop);
 
-    /*
-    ShowType<
-        KripkeModel<
-            List<Node<1>, Node<2>, Node<3>>,
-            List<Node<1>>,
-            AdjList<
-                MapItem<Node<1>, List<Node<2>, Node<3>>>,
-                MapItem<Node<3>, List<Node<2>>>
-            >,
-            Dict<
-                MapItem<Node<1>, List<decltype("p"_prop), decltype("q"_prop)>>,
-                MapItem<Node<2>, List<decltype("p"_prop)>>
-            >
+    using formula = EX<Or<EG<Or<p, Not<q>>>, r>>;
+    using model = KripkeModel<
+        List<Node<1>, Node<2>, Node<3>>,
+        List<Node<1>>,
+        AdjList<
+            MapItem<Node<1>, List<Node<2>, Node<3>>>,
+            MapItem<Node<3>, List<Node<2>>>
+        >,
+        DefaultDict<List<>,
+            MapItem<Node<1>, List<p, q>>,
+            MapItem<Node<2>, List<r>>
         >
-    > _;
-    */
+    >;
+
+    using result = typename CTLCheck<model, Or<q, Not<r>>>::Satisfy;
+    ShowType<result> _;
 }
